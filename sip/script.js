46477120a9cbc637d611db4c095205356a042c55
Chirag Gandherva <chiraggandherva08@gmail.com>
2023-08-25 12:59:05 +0530

    updates some files

--- sip/script.js
+++ sip/script.js
@@ -14,8 +14,22 @@ percentReturnRange.addEventListener("change", () => {
     percentReturn.value = percentReturnRange.value;
 })
 
+// for taking user input from time period.
+const timePeriod = document.querySelector("#time-period");
+const timePeriodRange = document.querySelector("#time-period-range");
+
+timePeriodRange.addEventListener("change", () => {
+    timePeriod.value = timePeriodRange.value;
+})
+
 setInterval(() => {
     monthlyInvRange.value = monthlyInv.value;
     percentReturnRange.value = percentReturn.value;
+    timePeriodRange.value = timePeriod.value;
 }, 2000);
 
+setInterval(() => {
+    document.querySelector("#investment-am-result").value = monthlyInv.value * ((1 + percentReturn/10));
+    document.querySelector("#est-ret-result").value = "";
+    document.querySelector("#total-val-result").value = "";
+}, 200)
